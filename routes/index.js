289a35c4fb8ef9171f4c474c06a8dc6864e3b98f
Indunil <inktwom@gmail.com>
2024-12-30 21:24:12 +0530

    User data display

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,41 @@
 var express = require('express');
 var router = express.Router();
-var connection = require ('../config/db');
+var connection = require('../config/db');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Sale! Sale! Sale!' });
 });
 
-router.post('/saveFormData', function(req,res){
-  const userData ={
-    fullName : req.body.txt_full_name,
-    email : req.body.txt_email,
-    contact : req.body.txt_contact,
-    description : req.body.txt_description,
-    isactive : 1,
+// /getUsersData
+router.get('/getUsersData', function (req, res, next) {
+  connection.query('SELECT * FROM `tbl_node_crud` ORDER BY `tbl_node_crud`.`user_id` ASC ', function (err, rows) {
+    if (err) {
+      console.log(err);
+      res.status(500).send({ message: "Internal Error !!!", status: 500 });
+    } else {
+      console.log(rows);
+      res.json(rows); // Send rows as JSON
+    }
+  });
+});
+
+
+// create user
+router.post('/saveFormData', function (req, res) {
+  const userData = {
+    fullName: req.body.txt_full_name,
+    email: req.body.txt_email,
+    contact: req.body.txt_contact,
+    description: req.body.txt_description,
+    isactive: 1,
   };
   console.log(userData);
-  connection.query("INSERT INTO tbl_node_crud SET? ", userData, function(err,result){
-    if(err) {
+  connection.query("INSERT INTO tbl_node_crud SET? ", userData, function (err, result) {
+    if (err) {
       console.log(err, '{"message" : "Internal Error !!!", "status" : 500}');
       res.end('{"message" : "Internal Error !!!", "status" : 500}');
-    }else {
+    } else {
       console.log('Record Successfully Save');
       console.log(userData);
       res.end('{"message" : "Record Created Successfully ...", "status" : 200}');
